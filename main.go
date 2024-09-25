a395ba941c525ebc9d945c09df1e66ecc6fc3e03
masa5555 <masa57887@gmail.com>
2024-09-25 17:52:50 +0900

    送信内容をすべてのクライアントに配信する

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -29,6 +30,7 @@ func serveHTML(w http.ResponseWriter, r *http.Request) {
 }
 
 var dummyDB = []string{}
+var connMemory = []net.Conn{}
 
 func chatHandler(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
@@ -62,6 +64,7 @@ func chatHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
+	connMemory = append(connMemory, conn)
 
 	// ハンドシェイクレスポンスの送信
 	bufrw.WriteString("HTTP/1.1 101 Switching Protocols\r\n")
@@ -93,85 +96,100 @@ func chatHandler(w http.ResponseWriter, r *http.Request) {
 			+---------------------------------------------------------------+
 	*/
 
-	// サーバーからはマスクしない。
-	var sendFirstByte byte = 0b1000_0001
 	for i := 0; i < len(dummyDB); i++ {
-		fmt.Println("get 0: ", dummyDB[i])
-		var dummyDBrow = []byte(dummyDB[i])
-		var payloadLength = len(dummyDBrow)
-		fmt.Printf("data: %s length: %d\n", dummyDB[i], payloadLength)
-		var secondByte = byte(0b0000_0000 ^ payloadLength)
-		fmt.Printf("2nd byte: %#08b\n", secondByte)
-		var sendBytes = append([]byte{sendFirstByte, secondByte}, dummyDBrow...)
-		for _, by := range sendBytes {
-			fmt.Printf("%#08b ", by)
-		}
-		fmt.Print("\n")
-		code, err := conn.Write(sendBytes)
-		if err != nil {
-			fmt.Println("Write error")
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("send code: %d\n", code)
+		send(conn, dummyDB[i])
 	}
 
-	var firstByte = make([]byte, 1)
-	_, err = conn.Read(firstByte)
-	if err != nil {
-		fmt.Println("Read error")
-		fmt.Println(err)
-		return
-	}
-
-	// FIN (1bit): 終了フレームかどうか
-	var fin = firstByte[0] >> 7
-	// OPCODE (4bits): %x0 … 継続フレーム, %x1 … テキストフレーム, %x2 … バイナリフレーム
-	//		%x3-7 … それ以外の非制御フレームで予約されている
-	//		%x8 … 接続のクローズ, %x9 … ping, %xA … pong
-	// 	  %xB-F … 以降の制御フレーム用に予約されている
-	var opcode = firstByte[0] & 0x0F
-	fmt.Printf("1st byte: %#08b fin: %d, opcode %d\n", firstByte[0], fin, opcode)
-
-	var secondByte = make([]byte, 1)
-	_, err = conn.Read(secondByte)
-	if err != nil {
-		fmt.Println("Read error")
-		fmt.Println(err)
-		return
-	}
-
-	// MASK (1bit): ペイロードがマスクされているかどうか
-	var mask = secondByte[0] >> 7
-	// Payload length (7bits): ペイロードの長さ
-	var payloadLength = secondByte[0] & 0x7F
-	fmt.Printf("2nd byte: %#08b mask: %d, payloadLength: %d\n", secondByte[0], mask, payloadLength)
-
-	// Masking-key: 4 bytes
-	var maskKey = make([]byte, 4)
-	_, err = conn.Read(maskKey)
-	if err != nil {
-		fmt.Println("Read error")
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("Masking-key: %#08b %#08b %#08b %#08b\n", maskKey[0], maskKey[1], maskKey[2], maskKey[3])
-
-	// Payload data　{PayloadLength} bytes
-	fmt.Printf("unmask data: ")
-	var payloadBytes = make([]byte, payloadLength)
-	for i := 0; i < int(payloadLength); i++ {
-		data := make([]byte, 1)
-		_, err = conn.Read(data)
+	for {
+		var firstByte = make([]byte, 1)
+		_, err = conn.Read(firstByte)
 		if err != nil {
 			fmt.Println("Read error")
 			fmt.Println(err)
+			return
 		}
-		payloadBytes[i] = data[0] ^ maskKey[i%4]
-		fmt.Printf("%#08b ", payloadBytes[i])
+
+		// FIN (1bit): 終了フレームかどうか
+		var fin = firstByte[0] >> 7
+		// OPCODE (4bits): %x0 … 継続フレーム, %x1 … テキストフレーム, %x2 … バイナリフレーム
+		//		%x3-7 … それ以外の非制御フレームで予約されている
+		//		%x8 … 接続のクローズ, %x9 … ping, %xA … pong
+		// 	  %xB-F … 以降の制御フレーム用に予約されている
+		var opcode = firstByte[0] & 0x0F
+		fmt.Printf("1st byte: %#08b fin: %d, opcode %d\n", firstByte[0], fin, opcode)
+
+		// テキストフレームかつ終了フレームのみ処理する
+		if fin != 1 || opcode != 1 {
+			fmt.Println("fin or opcode exception")
+			return
+		}
+
+		var secondByte = make([]byte, 1)
+		_, err = conn.Read(secondByte)
+		if err != nil {
+			fmt.Println("Read error")
+			fmt.Println(err)
+			return
+		}
+
+		// MASK (1bit): ペイロードがマスクされているかどうか
+		var mask = secondByte[0] >> 7
+		// Payload length (7bits): ペイロードの長さ
+		var payloadLength = secondByte[0] & 0x7F
+		fmt.Printf("2nd byte: %#08b mask: %d, payloadLength: %d\n", secondByte[0], mask, payloadLength)
+
+		// Masking-key: 4 bytes
+		var maskKey = make([]byte, 4)
+		_, err = conn.Read(maskKey)
+		if err != nil {
+			fmt.Println("Read error")
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Masking-key: %#08b %#08b %#08b %#08b\n", maskKey[0], maskKey[1], maskKey[2], maskKey[3])
+
+		// Payload data　{PayloadLength} bytes
+		fmt.Printf("unmask data: ")
+		var payloadBytes = make([]byte, payloadLength)
+		for i := 0; i < int(payloadLength); i++ {
+			data := make([]byte, 1)
+			_, err = conn.Read(data)
+			if err != nil {
+				fmt.Println("Read error")
+				fmt.Println(err)
+			}
+			payloadBytes[i] = data[0] ^ maskKey[i%4]
+			fmt.Printf("%#08b ", payloadBytes[i])
+		}
+		fmt.Printf("\n")
+		fmt.Printf("Raw data: %s\n", payloadBytes)
+		dummyDB = append(dummyDB, string(payloadBytes))
+		fmt.Printf("dummyDB: %v\n\n", dummyDB)
+		for _, c := range connMemory {
+			send(c, string(payloadBytes))
+		}
 	}
-	fmt.Printf("\n")
-	fmt.Printf("Raw data: %s\n", payloadBytes)
-	dummyDB = append(dummyDB, string(payloadBytes))
-	fmt.Printf("dummyDB: %v\n\n", dummyDB)
+}
+
+func send(conn net.Conn, data string) {
+	// サーバーからはマスクしない。
+	var sendFirstByte byte = 0b1000_0001
+	fmt.Println("get 0: ", data)
+	var dummyDBrow = []byte(data)
+	var payloadLength = len(dummyDBrow)
+	fmt.Printf("data: %s length: %d\n", data, payloadLength)
+	var secondByte = byte(0b0000_0000 ^ payloadLength)
+	fmt.Printf("2nd byte: %#08b\n", secondByte)
+	var sendBytes = append([]byte{sendFirstByte, secondByte}, dummyDBrow...)
+	for _, by := range sendBytes {
+		fmt.Printf("%#08b ", by)
+	}
+	fmt.Print("\n")
+	code, err := conn.Write(sendBytes)
+	if err != nil {
+		fmt.Println("Write error")
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("send code: %d\n", code)
 }
